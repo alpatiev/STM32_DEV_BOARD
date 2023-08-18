d873831b8364c8645025dfb03b449c383f3082cf
Alpatiev Nikita <chococacaococonut@gmail.com>
2023-08-18 21:59:34 +0300

    led line (5 by 1)

--- SOURCES/CORE/main.c
+++ SOURCES/CORE/main.c
@@ -1,23 +1,88 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
+static int delay = 600000;
 
-int main(void) {
+static void clock_setup(void)
+{
+	/* Set STM32 to 64 MHz. */
+	rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
+
+	/* Enable alternate function peripheral clock. */
+	rcc_periph_clock_enable(RCC_AFIO);
+
+	/* Enable GPIOB clock for led lamps. */
+	rcc_periph_clock_enable(RCC_GPIOB);
+
+	/* 
+	Enable GPIOB clock for embedded led lamps (user led) 
 	rcc_periph_clock_enable(RCC_GPIOC);
+	*/
+}
 
-	gpio_set_mode (
-		GPIOC,
-		GPIO_MODE_OUTPUT_2_MHZ,
-		GPIO_CNF_OUTPUT_PUSHPULL,
-		GPIO13
-	);
+static void gpio_setup(void)
+{
+	/* Configure PB4 as GPIO. */
+	AFIO_MAPR |= AFIO_MAPR_SWJ_CFG_FULL_SWJ_NO_JNTRST;
+	
+	/* Set GPIOs (in GPIO port B) to 'output push-pull'. */
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO4 | GPIO5 | GPIO6 | GPIO7 | GPIO8);
+
+	/* 
+	Set GPIO13 to 'output push-pull' for embedded led. 
+	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+	*/
+}
+
+static void cpu_pause(int timeout)
+{
+	for (int i = 0; i < timeout; i++) {
+		__asm__("nop");
+	}
+}
+
+
+int main(void)
+{
+	clock_setup();
+	gpio_setup();
+
+	gpio_toggle(GPIOB, GPIO4);
 
 	while (1) {
-		for (int i = 0; i < 1000000; i++) {
-		__asm__("nop");
-		}
+		gpio_toggle(GPIOB, GPIO4);
 
-		gpio_toggle(GPIOC, GPIO13);
+		cpu_pause(delay);
+
+		//		gpio_toggle(GPIOB, GPIO4);
+
+		gpio_toggle(GPIOB, GPIO5);
+
+		cpu_pause(delay);
+
+		//		gpio_toggle(GPIOB, GPIO5);
+
+
+		gpio_toggle(GPIOB, GPIO6);
+
+		cpu_pause(delay);
+
+		//		gpio_toggle(GPIOB, GPIO6);
+
+
+		gpio_toggle(GPIOB, GPIO7);
+
+		cpu_pause(delay);
+
+		//		gpio_toggle(GPIOB, GPIO7);
+
+
+		gpio_toggle(GPIOB, GPIO8);
+
+		cpu_pause(delay);
+
+		//		gpio_toggle(GPIOB, GPIO8);
+
 	}
 
 	return 0;
